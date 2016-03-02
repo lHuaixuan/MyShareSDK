7fa3d148a9ae7c123749336fc7776744b3263833
lhx <2449100900@qq.com>
2016-03-02 15:01:25 +0800

    Initial Commit

---
+++ MyShareSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyShareSDK
+//
+//  Created by admin on 16/3/2.
+//  Copyright © 2016年 刘怀轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
